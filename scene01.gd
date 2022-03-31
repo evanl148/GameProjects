61fcde792d1c04f8c3465e316dcc8dc575eac0cc
Phinkurniawan <2072035@maranatha.ac.id>
2022-03-31 09:18:52 +0700

    commit

--- scene01.gd
+++ scene01.gd
@@ -11,7 +11,6 @@ var laju = 150
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 func _physics_process(delta): 
 	mons1.flip_h = true
 	if (mons.get_offset()>900):
